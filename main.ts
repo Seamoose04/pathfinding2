e8beecd2bfa7b7c4180fa25d302f959aae16935d
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2024-01-27 13:20:53 -0500

    Fixed (?) pathfinding origin initialization

--- main.ts
+++ main.ts
@@ -75,7 +75,7 @@ class PathFinder {
     }
 
     updatePathfinding(target: Sprite) {
-        this.origin = new Node(new Vector2(this.sprite.x, this.sprite.y), null);
+        this.origin = new Node(new Vector2(target.tilemapLocation().col, target.tilemapLocation().row), null);
         this.origin.G = 0;
         if (Vector2.distance(new Vector2(this.followTarget.x, this.followTarget.y), new Vector2(this.sprite.x, this.sprite.y)) < 5) {
             let path = this.findPath(new Vector2(target.tilemapLocation().col, target.tilemapLocation().row));
