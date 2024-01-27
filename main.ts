436666000e7d771ac4ab153110d9f75312835585
Seamus Johnson <72893263+Seamoose04@users.noreply.github.com>
2024-01-27 17:41:46 -0500

    Update main.ts

--- main.ts
+++ main.ts
@@ -75,13 +75,10 @@ class PathFinder {
     }
 
     updatePathfinding(target: Sprite) {
-        this.origin = new Node(new Vector2(target.tilemapLocation().col, target.tilemapLocation().row), null);
+        this.origin = new Node(new Vector2(this.sprite.tilemapLocation().col, this.sprite.tilemapLocation().row), null);
         this.origin.G = 0;
         if (Vector2.distance(new Vector2(this.followTarget.x, this.followTarget.y), new Vector2(this.sprite.x, this.sprite.y)) < 5) {
             let path = this.findPath(new Vector2(target.tilemapLocation().col, target.tilemapLocation().row));
-            console.log(target.x);
-            console.log(this.sprite.x);
-            console.log(this.followTarget.x);
             if (path) {
                 let next = path[path.length - 1];
                 tiles.placeOnTile(this.followTarget, tiles.getTileLocation(next.x, next.y));
